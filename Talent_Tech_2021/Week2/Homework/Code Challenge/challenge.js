25a49c742bc1cfd2842d7ad89ef91393c8ee96bf
Maurice Ruffin <Tysons3x3@gmail.com>
2021-04-15 21:26:44 -0400

    new commit

---
+++ Talent_Tech_2021/Week2/Homework/Code Challenge/challenge.js
@@ -0,0 +1,16 @@
+var cups = 1;
+
+if (cups < 3) {
+  console.log("Yes. I'll take another cup of coffee!");
+} else {
+  console.log("I think I'm okay for now.");
+}
+
+var car = 1;
+var oil = 500;
+
+if (car < 500) {
+  console.log("Yes I think I need an oil Change");
+} else {
+  console.log("My car doesn't need an oil change yet.");
+}
